ef6bfb172dc6fc34c9a8df94439c2ffce37636e0
0xSakCodes <itsaksham.dev@gmail.com>
2026-05-23 14:58:23 +0530

    update: fgets/puts;

--- basicPrograms/program50.c
+++ basicPrograms/program50.c
@@ -9,9 +9,19 @@ int main() {
     char firstName[10];
     char fullName[25];
 
-    // taking user input %s;
+    // taking user input %s; 
+    /// these types of input dont support multi-words input;
     printf("enter your first name: ");
-    scanf("%s", firstName);
+    //scanf("%s", firstName);
+
+    // in such scenarios, we must use (fgets -> input); (puts -> output);
+    // followed by: fgets(string, maxLength, stdin);
+    printf("enter your full name: ");
+    fgets(fullName, 25, stdin);
+    puts(fullName);
+    // string -> variable where string will be stored;
+    // maxLength -> maximum character input allowed; 
+    // stdin -> standard input;
 
     // printing the name:
     printf("your first name: %s", firstName);
