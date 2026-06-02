5996e0eca80de14d8d90b26aada73639f0e82e01
0xSakCodes <itsaksham.dev@gmail.com>
2026-06-02 22:27:56 +0530

    update: file logic;

--- basicPrograms/program67.c
+++ basicPrograms/program67.c
@@ -13,13 +13,30 @@ int main() {
     | opening file;
     || in reading mode;
     || fopen(location, mode);
+    || if file doesn't exist NULL will be passed to the variable;
     */
     fptr = fopen("./programFiles/test.txt", "r");
 
     /* 
     | closing the opened file;
     || fclose(fileLoc);
+    ||| creating a condition to check if the file exists before we perform operations on it;
     */
-    fclose(fptr);
+    if (fptr != NULL) {
+        /*
+        | define a variable to store file values;
+        || usage of fscanf(fileLoc, dataType, &variable);
+        || fileLoc -> variable of opened file;
+        || dataType -> data type in which you are willing to access data;
+        || &variable -> address of variable where the fetched data would be stored;
+        */
+        char fileData[20];
+        fscanf(fptr, "%s", &fileData);
+        // prints the data;
+        printf("string: %s", fileData);
+        fclose(fptr);
+    } else {
+        printf("file not found :(");
+    }
     return 0;
 }
