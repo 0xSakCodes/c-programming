c747f270c5a49b917de781974df851d5ca06f75e
0xSakCodes <itsaksham.dev@gmail.com>
2026-06-02 12:24:38 +0530

    update: logical fixes;

--- basicPrograms/program64.c
+++ basicPrograms/program64.c
@@ -8,40 +8,45 @@ typedef struct addition {
     | x -> i cap;
     | y -> j cap;
     || a1 & a2 + b1 & b2;
-    */
+    
     int a1, a2;
     int b1, b2;
+    */
     int x, y;
 } add;
 
 // user defined function declaration:
-void addition(add vector);
+void addition(add vector1, add vector2, add sumOfVector);
 
 // main function:
 int main() {
-   add vector;
+   add vector1;
+   add vector2;
+   add sumOfVector;
+
    // first eqn;
    printf("--- first eqn ---\n");
    printf("enter value of i(cap): ");
-   scanf("%d", &vector.a1);
+   scanf("%d", &vector1.x);
    printf("enter value of j(cap): ");
-   scanf("%d", &vector.a2);
+   scanf("%d", &vector1.y);
+
    // second eqn;
    printf("\n\n--- second eqn ---\n");
    printf("enter value of i(cap): ");
-   scanf("%d", &vector.b1);
+   scanf("%d", &vector2.x);
    printf("enter value of j(cap): ");
-   scanf("%d", &vector.b2);
+   scanf("%d", &vector2.y);
 
    printf("\n\n--- vector addition ---\n");
-   addition(vector);
+   addition(vector1, vector2, sumOfVector);
     return 0;
 }
 
 // defining function: 
-void addition(add vector) {
-    vector.x = vector.a1 + vector.b1;
-    vector.y = vector.a2 + vector.a2;
+void addition(add vector1, add vector2, add sumOfVector) {
+    sumOfVector.x = vector1.x + vector2.x;
+    sumOfVector.y = vector1.y + vector2.y;
 
-    printf("%d i(cap)\t%d j(cap)", vector.x, vector.y);
+    printf("%d i(cap)\t%d j(cap)", sumOfVector.x, sumOfVector.y);
 }
