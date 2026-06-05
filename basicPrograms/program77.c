b8bbc92dd21f12abca2918de4586199306b3a8f0
0xSakCodes <itsaksham.dev@gmail.com>
2026-06-05 23:40:34 +0530

    update: additional patterns;

--- basicPrograms/program77.c
+++ basicPrograms/program77.c
@@ -27,11 +27,46 @@ void printPattern(int numOfRows) {
     }
 }
 
+/*
+| function to print number pattern by number of rows;
+|| 1
+|| 1 2
+|| 1 2 3
+|| 1 2 3 4
+*/
+void printNumberPattern(int numberOfRows) {
+    for (int i=1; i<=numberOfRows; i++) {
+        for (int j=1; j<=i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
+
+/*
+| function to print number reversed pattern by number of rows;
+|| 1 2 3 4
+|| 1 2 3
+|| 1 2 
+|| 1 
+*/
+void printReversedNumPattern(int numberOfRows) {
+    // 4, 3, 2, 1,
+    for (int i=numberOfRows; i>0; i--) {
+        for (int j=1; j<=i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     int numOfRows;
     printf("enter number of rows: ");
     scanf("%d", &numOfRows);
 
     printPattern(numOfRows);
+    printNumberPattern(numOfRows);
+    printReversedNumPattern(numOfRows);
     return 0;
 }
