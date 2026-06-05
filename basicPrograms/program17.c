1d23f018159fed649d7a20f4c4918c50c3986e11
0xSakCodes <111876198+0xSakCodes@users.noreply.github.com>
2026-06-05 19:05:01 +0530

    update: complete pattern;

--- basicPrograms/program17.c
+++ basicPrograms/program17.c
@@ -1,1 +1,31 @@
 // WAP to print pattern via nested loop:
+/// (05/06/2026) {0xSakCodes}
+
+#include <stdio.h>
+
+// user defined function declaration:
+void printPattern(char spChar, int lines) {
+  // logic
+  for (int i=1; i<=lines; i++) {
+    // 5 chars to be printed in each line
+    for (int j=1; j<=5; j++) {
+      printf("%c", spChar);
+    }
+    printf("\n");
+  }
+}
+
+// main function
+int main() {
+  char spchar;
+  int lines;
+  
+  printf("enter special character: ");
+  scanf("%c", &spchar);
+  printf("enter number of lines: ");
+  scanf("%d", &lines);
+
+  // calling function
+  printPattern(spchar, lines);
+  return 0;
+}
